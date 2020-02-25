dfe3d0cd66ff60679f1287f7f8076e4f5dce31e3
ignacio68 <ilopez-amor@ya.com>
2020-02-25 18:00:31 +0100

    Add locales

---
+++ app/locales/en_US/views.js
@@ -0,0 +1,5 @@
+export default {
+    welcome: {
+        header: 'WELCOME TO COVID-19 APP'
+    }
+}
---
+++ app/locales/es_ES/views.js
@@ -0,0 +1,5 @@
+export default {
+    welcome: {
+        header: 'BIENVENIDO A COVID-19 APP'
+    }
+}
