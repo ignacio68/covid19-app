5203f7f99032bc94cdcdf14560d4db8c7d3b0b3d
Ignacio <buscemi@ya.com>
2020-02-26 10:27:41 +0100

    Rename component

---
+++ app/components/Main/MapComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <AbsoluteLayout class="map">
+    <MapBox
+      left="0"
+      top="0"
+      width="100%"
+      height="100%"
+      :accessToken="accessToken"
+      mapStyle="traffic_night"
+      :latitude="userLatitude"
+      :longitude="userLongitude"
+      zoomLevel="5"
+    ></MapBox>
+  </AbsoluteLayout>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+// import { Mapbox } from 'nativescript-mapbox'
+
+@Component({
+  // components: {
+  //   Mapbox
+  // }
+})
+export default class Map extends Vue {
+  @Prop() accessToken: string
+  @Prop({default: "45.137451890638886"}) userLatitude: string
+  @Prop({default: "-68.13734351262877"}) userLongitude: string
+}
+</script>
