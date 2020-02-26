8eebda98a69ebae6874da09bf392c73e971a30bb
ignacio68 <ilopez-amor@ya.com>
2020-02-26 18:06:26 +0100

    Change export mode

--- app/locales/en_US/views.ts
+++ app/locales/en_US/views.ts
@@ -1,7 +1,5 @@
-declare const views =  {
-    welcome: {
-        header: 'WELCOME TO COVID-19 APP'
-    }
+export const views = {
+  welcome: {
+    header: 'WELCOME TO COVID-19 APP'
+  }
 }
-
-export default views
