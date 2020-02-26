210e038978b7077a57aff748b9163e2735111b9d
Ignacio <buscemi@ya.com>
2020-02-26 10:28:16 +0100

    New views

---
+++ app/views/Main/Info.vue
@@ -0,0 +1,11 @@
+<template>
+  <StackLayout horizontalAligment="center">
+    <Label class="h1" text="Info Page" textWrap="true" />
+  </StackLayout>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class Map extend Vue {
+}
+</script>
---
+++ app/views/Main/Map.vue
@@ -0,0 +1,19 @@
+<template>
+  <StackLayout>
+    <Map
+      :accessToken="accessToken"
+      userLatitude="45.137451890638886"
+      userLongitude="-68.13734351262877"
+    />
+  </StackLayout>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import MapComponent from './Main/Map.vue'
+@Component ({
+  Map: MapComponent
+})
+export default class Map extend Vue {
+  private accessToken = mapboxToken
+}
+</script>
