368da757507529b4cda5762017b6e550ef2cc255
ignacio68 <ilopez-amor@ya.com>
2020-02-26 08:36:57 +0100

    Add Covid-19 database

---
+++ app/utils/covid19.js
@@ -0,0 +1,30 @@
+
+export const covid19 = {
+  _id: '20200225',
+  type: 'covid19',
+  europe: {
+    italy: {
+      cases: '48',
+      deaths: '0',
+      confirmed: '',
+      suspected: '',
+      transmited: '',
+      imported: '',
+      totalCases: '124',
+      totalDeaths: '2'
+    }
+  }
+}
+
+// europe: {
+//     italia: {
+//       cases: '',
+//       deaths: '',
+//       confirmed: '',
+//       suspected: '',
+//       transmited: '',
+//       imported: '',
+//       totalCases: '',
+//       totalDeaths: ''
+//     }
+//   }
