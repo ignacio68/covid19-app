e562c3ee64fd2c6e8f763abdc8154aa85509a0b4
Ignacio <buscemi@ya.com>
2020-02-27 15:30:22 +0100

    Population for test

--- app/utils/covid19.ts
+++ app/utils/covid19.ts
@@ -1,18 +1,36 @@
 
 export const covid19 = {
-  _id: '20200225',
+  _id: '20200226',
   type: 'covid19',
   europe: {
-    italy: {
-      cases: '48',
+    IT: {
+      confirmed: '93',
+      suspected: '93',
+      deaths: '5',
+      totalConfirmed: '322',
+      totalDeaths: '11'
+    },
+    ES: {
+      confirmed: '0',
+      suspected: '0',
       deaths: '0',
-      confirmed: '',
-      suspected: '',
-      transmited: '',
-      imported: '',
-      totalCases: '124',
-      totalDeaths: '2'
-    }
+      totalConfirmed: '2',
+      totalDeaths: '0'
+    },
+    FR: {
+      confirmed: '0',
+      suspected: '0',
+      deaths: '0',
+      totalConfirmed: '12',
+      totalDeaths: '0'
+    },
+    UK: {
+      confirmed: '0',
+      suspected: '0',
+      deaths: '0',
+      totalConfirmed: '13',
+      totalDeaths: '1'
+    },
   }
 }
 
