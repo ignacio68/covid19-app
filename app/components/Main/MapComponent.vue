061a03de461a802686dded0a280b359e05bf9ef9
ignacio68 <ilopez-amor@ya.com>
2020-02-29 11:43:54 +0100

    Add countries names to locales

--- app/components/Main/MapComponent.vue
+++ app/components/Main/MapComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <AbsoluteLayout class="map">
     <MapBox
+      ref="map"
       left="0"
       top="0"
       width="100%"
@@ -9,7 +10,7 @@
       mapStyle="traffic_night"
       :latitude="userLatitude"
       :longitude="userLongitude"
-      zoomLevel="5"
+      zoomLevel="zoomLevel"
       :hideCompass="true"
       :disableRotation="true"
       :mapReady="onMapReady()"
@@ -28,8 +29,10 @@ import { Component, Prop, Emit, Vue } from 'vue-property-decorator'
 })
 export default class MapComponent extends Vue {
   @Prop(String) readonly accessToken!: string | undefined
+  @Prop({default: "5"}) readonly zoomLevel!: string
   @Prop({default: "45.137451890638886"}) readonly userLatitude!: string
   @Prop({default: "-68.13734351262877"}) readonly userLongitude!: string
+
 
   @Emit('onMapReady')
   onMapReady(){}
