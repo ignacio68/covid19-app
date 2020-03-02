44de9c79f59b2a6d5f36e4e78d381c85d4f1c559
ignacio68 <ilopez-amor@ya.com>
2020-03-02 18:12:17 +0100

    Add font icon component & plugin

---
+++ app/components/Core/FontIcon.vue
@@ -0,0 +1,41 @@
+<template>
+  <Label
+    :class="type"
+    :color="color"
+    :fontSize="fontSize"
+    :text="name | fonticon"
+    :backgroundColor="backgroundColor"
+    textAlignment="center"
+    @tap="$emit('tap')"
+  />
+</template>
+<script>
+
+export default {
+  name: 'FontIcon',
+  props: {
+    type: {
+      type: String,
+      required: true,
+      default: 'fa'
+    },
+    color: {
+      type: String,
+      default: 'black'
+    },
+    fontSize: {
+      type: [String, Number],
+      default: 24,
+      validation: s => !isNaN(s)
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    backgroundColor: {
+      type:String,
+      default: 'transparent'
+    }
+  }
+}
+</script>
