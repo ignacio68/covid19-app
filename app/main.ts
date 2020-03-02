ef6874ecaaff08d1ffcd42dce46d5fe83b4617c6
ignacio68 <ilopez-amor@ya.com>
2020-03-02 18:16:15 +0100

    Add font icon

--- app/main.ts
+++ app/main.ts
@@ -11,6 +11,9 @@ import Vue from "nativescript-vue"
 
 import { device, isAndroid, isIOS } from "tns-core-modules/platform"
 
+// Add NativeScript plugin
+import { TNSFontIcon, fonticon } from 'nativescript-fonticon'
+
 import store from "./store"
 
 // Import languages
@@ -25,6 +28,17 @@ import AppNavigator from "./views/AppNavigator.vue"
 if (TNS_ENV !== "production") {
   Vue.use(VueDevtools)
 }
+
+// Load TNSFonticon
+TNSFontIcon.debug = true
+TNSFontIcon.paths = {
+  fa: './assets/css/fontawesome.css',
+  far: './assets/css/regular.css',
+  fas: './assets/css/solid.css',
+  fab: './assets/css/brand.css'
+}
+TNSFontIcon.loadCss()
+Vue.filter('fonticon', fonticon)
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = TNS_ENV === "production"
