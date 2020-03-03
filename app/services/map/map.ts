7535c56885703b17f0c8ea4e6915cb1d83816c70
Ignacio <buscemi@ya.com>
2020-03-03 13:56:27 +0100

    Add new functions

--- app/services/map/map.ts
+++ app/services/map/map.ts
@@ -1,17 +1,26 @@
 import { getCountry } from '@/services/countries'
 import { getCovid19CountryData } from '@/services/covid19'
 import { countriesList } from '@/repository'
-import { IMarker } from './interfaces'
+import { marker } from './interfaces'
+
+const allCountryMarkers: Array<any> = []
 
 function setMarker(countryCode: string){
   const countryData = getCountry(countryCode)
   // const covid19CountryData = getCovid19CountryData(countryCode)
   // const marker = { ...countryData, ...covid19CountryData }
   const marker = countryData
+  console.log(`marker: + ${JSON.stringify(marker)}`)
   return marker
 }
 
+function setAllMarkers(countryCode) {
+  let marker = setMarker(countryCode)
+  allCountryMarkers.push(marker)
+}
+
 export function getMarkers() {
-  const countriesMarkers = countriesList.forEach(code => setMarker("code"))
+  const countriesMarkers = countriesList.forEach(item => setAllMarkers(item["code"]))
+  console.log(`marker: + ${JSON.stringify(countriesMarkers)}`)
   return countriesMarkers
 }
