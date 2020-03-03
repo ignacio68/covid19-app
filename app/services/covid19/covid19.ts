cc39df57270ed916c4156ce2f883a8030bf6fc63
ignacio68 <ilopez-amor@ya.com>
2020-03-03 23:16:07 +0100

    refactored get country data functions

--- app/services/covid19/covid19.ts
+++ app/services/covid19/covid19.ts
@@ -2,13 +2,15 @@ import { covid19DataList } from '@/repository'
 import { covid19Data } from './interfaces'
 
 export function getCovid19CountryData(countryCode: string) {
-  const getCovid19CountryData = covid19DataList.filter(covid19DataListed => covid19DataListed._id === countryCode)
-  const covid19CountryData: covid19Data = {
-    confirmed: getCovid19CountryData['confirmed'],
-    suspected: getCovid19CountryData['suspected'],
-    deaths: getCovid19CountryData['deaths'],
-    totalConfirmed: getCovid19CountryData['totalConfirmed'],
-    totalDeaths: getCovid19CountryData['totalDeaths']
-  }
+  const getCovid19CountryData = covid19DataList.filter(covid19DataListed => covid19DataListed.id === countryCode)
+
+  const covid19CountryData: covid19Data = {}
+
+  // covid19CountryData.confirmed = getCovid19CountryData[0].confirmed,
+  // covid19CountryData.suspected = getCovid19CountryData[0].suspected,
+  // covid19CountryData.deaths = getCovid19CountryData[0].deaths,
+  // covid19CountryData.totalConfirmed = getCovid19CountryData[0].totalConfirmed,
+  covid19CountryData.subtitle = `muertos: ${getCovid19CountryData[0].totalDeaths}`
+
   return covid19CountryData
 }
