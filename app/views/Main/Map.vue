2fecf1225fba92e0931bad0f05171d9c048ae3cc
ignacio68 <ilopez-amor@ya.com>
2020-03-03 21:14:12 +0100

    Resolve addMarkers minor bug

--- app/views/Main/Map.vue
+++ app/views/Main/Map.vue
@@ -3,7 +3,7 @@
     <StackLayout>
       <MapComponent
         :accessToken="accessToken"
-        zoomLevel="14"
+        :zoomLevel="3"
         userLatitude="39.8729940"
         userLongitude="-3.67089492"
         @onMapReady="showMarkers($event)"
@@ -28,22 +28,9 @@ export default class Map extends Vue {
   private accessToken: string = mapboxToken
 
   showMarkers(args) {
-    console.log('Estoy en showMarkers')
     console.log(console.dir(args))
     const markers = getMarkers()
-    args.map.addMarkers([
-      markers
-    ])
-    // args.map.addMarkers([
-    //   {
-    //     lat: 39.87299401,
-    //     lng: -3.67089492
-    //   },
-    //   {
-    //     lat: 46.48372145,
-    //     lng: 2.60926281
-    //   }
-    // ])
+    args.map.addMarkers(markers)
   }
 }
 </script>
