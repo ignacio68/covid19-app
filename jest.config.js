457eefdf730c341f16c6cf84214b36b40e2c47de
ignacio68 <ilopez-amor@ya.com>
2020-03-04 00:10:34 +0100

    add new configuration item

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
+  preset: 'ts-jest',
   roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 }
