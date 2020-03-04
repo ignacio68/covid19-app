8d10a64582666ad8d69f7ea8423f5baed3ec4e71
ignacio68 <ilopez-amor@ya.com>
2020-03-04 18:31:18 +0100

    improve: add Privacy Policy page

--- app/locales/en_US/views.ts
+++ app/locales/en_US/views.ts
@@ -2,6 +2,10 @@ export default {
   welcome: {
     title: 'WELCOME ABOUT COVID-19 APPLICATION'
   },
+  privacy: {
+    title: 'Privacy Policy',
+    main: 'Privacy Policy Page'
+  },
   main: {
     tabbar_map: 'MAP',
     tabbar_charts: 'CHARTS',
--- app/locales/es_ES/views.ts
+++ app/locales/es_ES/views.ts
@@ -2,6 +2,10 @@ export default {
   welcome: {
     title: 'BIENVENIDO/A A LA APLICACCIÓN SOBRE EL COVID-19'
   },
+  privacy: {
+    title: 'Política de Privacidad',
+    main: 'Página de la Política de Privacidad'
+  },
   main: {
     tabbar_map: 'MAPA',
     tabbar_charts: 'GRÁFICAS',
