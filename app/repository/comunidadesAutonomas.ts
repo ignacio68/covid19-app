6e86ce5914fd73c156965799f847cad45c2c6577
estudio <estudio@lopezamor.local>
2020-03-05 12:46:47 +0100

    improve: add Counidades Autonomas database

---
+++ app/repository/comunidadesAutonomas.ts
@@ -0,0 +1,112 @@
+export const comndadesAutonomas = [
+    {
+        name: "Andalucía",
+        lat: "37.366667",
+        lng: "-5.983333",
+    },
+    {
+        name: "Aragón",
+        lat: "41.65635",
+        lng: "-0.876566"
+    },
+    {
+        name: "Asturias",
+        oficialName: "Principado de Asturias",
+        lat: "43.36",
+        lng: "-5.845"
+    },
+    {
+        name: "Baleares",
+        oficialName:"Islas Baleares",
+        lat: "39.566667",
+        lng: "2.65"
+    },
+    {
+        name: "Canarias",
+        oficialNameame: "Islas Canarias",
+        lat: "28.15",
+        lng: "-15.416667",
+    },
+    {
+        name: "Cantabria",
+        lat: "43.45",
+        lng: "-3.8"
+    },
+    {
+        name: "Castilla-La Mancha",
+        lat: "39.856528",
+        lng: "-4.025111"
+    },
+    {
+        name: "Castilla y León",
+        lat: "41.633333",
+        lng: "-4.716667"
+    },
+    {
+        name: "Cataluña",
+        lat: "41.383333",
+        lng: "2.166667"
+    },
+    {
+        name: "Ceuta",
+        lat: "35.888056",
+        lng: "-5.316111"
+    },
+    {
+        name: "Extremadura",
+        lat: "38.9",
+        lng: "-6.333333"
+    },
+    {
+        name: "Galicia",
+        lat: "42.866667",
+        lng: "-8.55"
+    },
+    {
+        name: "La Rioja",
+        lat: "42.464653",
+        lng: "-2.4458"
+    },
+    {
+        name: "Andalucia",
+        lat: "37.3666",
+        lng: "-5.983"
+    },
+    {
+        name: "Madrid",
+        oficialName: "Comunidad de Madrid",
+        lat: "40.433333",
+        lng: "-3.683333"
+    },
+    {
+        name: "Melilla",
+        lat: "35.291667",
+        lng: "-2.938333"
+    },
+    {
+        name: "Murcia",
+        officialName: "Región de Murcia",
+        lat: "37.986111",
+        lng: "-1.130278"
+    },
+    {
+        name: "Navarra",
+        officialName: "Navarra",
+        lat: "42.818333",
+        lng: "-1.644167"
+    },
+    {
+        name: "Valencia",
+        officialName: "Comunidad Valenciana",
+        lat: "39.47",
+        lng: "-0.376667"
+    },
+    {
+        name: "País Vasco",
+        officialName: "País Vasco",
+        lat: "42.85",
+        lng: "-2.683333"
+    },
+  
+]
+    
