437f82aba36860880001b61958ace0c78a62ec46
ignacio68 <ilopez-amor@ya.com>
2020-03-08 10:32:01 +0100

    refactor code

--- app/services/covid19/covid19.ts
+++ app/services/covid19/covid19.ts
@@ -15,24 +15,8 @@ export async function getCovid19CountryData(countryCode: string) {
   // covid19CountryData.suspected = filterCovid19CountrData[0].suspected,
   // covid19CountryData.deaths = filterCovid19CountryData[0].deaths
   // covid19CountryData.totalConfirmed = filterCovid19CountrData[0].totalConfirmed,
-  const totalDeaths = filterCovid19CountryData[0].totalDeaths
-  console.log(`Muertos totales: ${totalDeaths}`)
-  covid19CountryData.subtitle = `muertos: ${totalDeaths}`
+  // const totalDeaths = filterCovid19CountryData[0].totalDeaths
+  console.log(`Muertos totales: ${filterCovid19CountryData[0].totalDeaths}`)
+  covid19CountryData.subtitle = `fallecidos: ${filterCovid19CountryData[0].totalDeaths}`
   return covid19CountryData
 }
-
-  // export function setCovid19CountryData(countryCode: string) {
-  //   const promise = new Promise((resolve, reject) => {
-  //   resolve(getCovid19CountryData(countryCode))
-  //   })
-  //   promise.then(country => {
-  //     const covid19CountryData: Covid19CountryData = {}
-  //     // covid19CountryData.confirmed = filterCovid19CountrData[0].confirmed,
-  //     // covid19CountryData.suspected = filterCovid19CountrData[0].suspected,
-  //     // covid19CountryData.deaths = filterCovid19CountryData[0].deaths
-  //     // covid19CountryData.totalConfirmed = filterCovid19CountrData[0].totalConfirmed,
-  //     const totalDeaths = country[0].totalDeaths
-  //     console.log(`Muertos totales: ${totalDeaths}`)
-  //     covid19CountryData.subtitle = `muertos: ${totalDeaths}`
-  //     return covid19CountryData
-  //   })
