ae9c3d2ba9153f87eaf2a5382363528b8c5dad4b
ignacio68 <ilopez-amor@ya.com>
2020-03-08 10:28:35 +0100

    refactor code

--- app/services/covid19/covid19.ts
+++ app/services/covid19/covid19.ts
@@ -1,18 +1,38 @@
 import { covid19CountriesDataList } from '@/repository'
 import { Covid19CountryData } from './interfaces'
 
-export function getCovid19CountryData(countryCode: string) {
-  const filterCovid19CountryData = covid19CountriesDataList.filter(countryData => {countryData.id === countryCode})
-  console.log(`filterCovid19CountrData: ${JSON.stringify(filterCovid19CountryData)}`)
+export async function getCovid19CountryData(countryCode: string) {
+  const filterCovid19CountryData = covid19CountriesDataList.filter(
+    country => country.code === countryCode
+  )
+  console.log(
+    `filterCovid19CountrData deaths: ${JSON.stringify(
+      filterCovid19CountryData
+    )}`
+  )
   const covid19CountryData: Covid19CountryData = {}
-
   // covid19CountryData.confirmed = filterCovid19CountrData[0].confirmed,
   // covid19CountryData.suspected = filterCovid19CountrData[0].suspected,
-  covid19CountryData.deaths = filterCovid19CountryData[0].deaths
+  // covid19CountryData.deaths = filterCovid19CountryData[0].deaths
   // covid19CountryData.totalConfirmed = filterCovid19CountrData[0].totalConfirmed,
-  // const totalDeaths = filterCovid19CountrData[0].totalDeaths
-  // console.log(`Muertos totales: ${totalDeaths}`)
-  // covid19CountryData.subtitle = `muertos: ${totalDeaths}`
-
+  const totalDeaths = filterCovid19CountryData[0].totalDeaths
+  console.log(`Muertos totales: ${totalDeaths}`)
+  covid19CountryData.subtitle = `muertos: ${totalDeaths}`
   return covid19CountryData
 }
+
+  // export function setCovid19CountryData(countryCode: string) {
+  //   const promise = new Promise((resolve, reject) => {
+  //   resolve(getCovid19CountryData(countryCode))
+  //   })
+  //   promise.then(country => {
+  //     const covid19CountryData: Covid19CountryData = {}
+  //     // covid19CountryData.confirmed = filterCovid19CountrData[0].confirmed,
+  //     // covid19CountryData.suspected = filterCovid19CountrData[0].suspected,
+  //     // covid19CountryData.deaths = filterCovid19CountryData[0].deaths
+  //     // covid19CountryData.totalConfirmed = filterCovid19CountrData[0].totalConfirmed,
+  //     const totalDeaths = country[0].totalDeaths
+  //     console.log(`Muertos totales: ${totalDeaths}`)
+  //     covid19CountryData.subtitle = `muertos: ${totalDeaths}`
+  //     return covid19CountryData
+  //   })
--- app/services/map/map.ts
+++ app/services/map/map.ts
@@ -1,19 +1,27 @@
 import { getCountry } from '@/services/countries'
 import { getCovid19CountryData } from '@/services/covid19'
 import { countriesList } from '@/repository'
-import { marker } from './interfaces'
+import { Marker } from './interfaces'
 import { MapboxMarker } from 'nativescript-mapbox'
 
 const allCountriesMarkers: Array<any> = []
 
 function setMarker(countryCode: string) {
   console.log('setMarker')
-  const countryData = getCountry(countryCode)
-  const covid19CountryData = getCovid19CountryData(countryCode)
-  const marker = { ...countryData, ...covid19CountryData }
-  // const marker = countryData
-  allCountriesMarkers.push(marker)
-  // console.log(`marker: + ${JSON.stringify(marker)}`)
+  let marker: Marker = {}
+  // const countryData = getCountry(countryCode)
+  getCovid19CountryData(countryCode)
+    .then(countryCovid19Data => {
+      marker = { ...marker, ...countryCovid19Data }
+      const countryData = getCountry(countryCovid19Data[0].code)
+    })
+    .then(country => {
+      const countryData = getCountry(country[0].code)
+      marker = { ...marker, ...countryData }
+      console.log(`marker: + ${JSON.stringify(marker)}`)
+      allCountriesMarkers.push(marker)
+    })
+    .catch ( error => console.log(`setMarker error: ${error}`))
 }
 
 export function getMarkers() {
