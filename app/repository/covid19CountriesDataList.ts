9f57ccc5b9ee31eadea01fbee73ee35ef6d4644c
ignacio68 <ilopez-amor@ya.com>
2020-03-08 10:27:21 +0100

    change prop name

--- app/repository/covid19CountriesDataList.ts
+++ app/repository/covid19CountriesDataList.ts
@@ -1,83 +1,34 @@
 export const covid19CountriesDataList = [
   {
-    id: 'IT',
-    confirmed: '93',
-    suspected: '93',
-    deaths: '5',
-    totalConfirmed: '322',
-    totalDeaths: '11'
+    code: 'IT',
+    confirmed: 93,
+    suspected: 93,
+    deaths: 5,
+    totalConfirmed: 322,
+    totalDeaths: 11
   },
   {
-    id: 'ES',
-    confirmed: '0',
-    suspected: '0',
-    deaths: '0',
-    totalConfirmed: '2',
-    totalDeaths: '0'
+    code: 'ES',
+    confirmed: 0,
+    suspected: 0,
+    deaths: 0,
+    totalConfirmed: 2,
+    totalDeaths: 0
   },
   {
-    id: 'FR',
-    confirmed: '0',
-    suspected: '0',
-    deaths: '0',
-    totalConfirmed: '12',
-    totalDeaths: '0'
+    code: 'FR',
+    confirmed: 0,
+    suspected: 0,
+    deaths: 0,
+    totalConfirmed: 12,
+    totalDeaths: 0
   },
   {
-    id: 'UK',
-    confirmed: '0',
-    suspected: '0',
-    deaths: '0',
-    totalConfirmed: '13',
-    totalDeaths: '1'
+    code: 'UK',
+    confirmed: 0,
+    suspected: 0,
+    deaths: 0,
+    totalConfirmed: 13,
+    totalDeaths: 1
   }
 ]
-
-// export const covid19CountriesDataList = {
-//   _id: 'fakeCovid19CountriesDataList',
-//   IT: {
-//     id: 'IT',
-//     confirmed: '93',
-//     suspected: '93',
-//     deaths: '5',
-//     totalConfirmed: '322',
-//     totalDeaths: '11'
-//   },
-//   ES: {
-//     id: 'ES',
-//     confirmed: '0',
-//     suspected: '0',
-//     deaths: '0',
-//     totalConfirmed: '2',
-//     totalDeaths: '0'
-//   },
-//   FR: {
-//     id: 'FR',
-//     confirmed: '0',
-//     suspected: '0',
-//     deaths: '0',
-//     totalConfirmed: '12',
-//     totalDeaths: '0'
-//   },
-//   UK: {
-//     id: 'UK',
-//     confirmed: '0',
-//     suspected: '0',
-//     deaths: '0',
-//     totalConfirmed: '13',
-//     totalDeaths: '1'
-//   }
-// }
-
-// europe: {
-//     italia: {
-//       cases: '',
-//       deaths: '',
-//       confirmed: '',
-//       suspected: '',
-//       transmited: '',
-//       imported: '',
-//       totalCases: '',
-//       totalDeaths: ''
-//     }
-//   }
