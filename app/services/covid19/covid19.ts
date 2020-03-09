fc3cd0f289a85cdb16c126b7ff722cec63f6f87a
ignacio68 <ilopez-amor@ya.com>
2020-03-10 00:22:02 +0100

    Add new prop, remove console tests and format subtitle text

--- app/services/covid19/covid19.ts
+++ app/services/covid19/covid19.ts
@@ -5,18 +5,15 @@ export async function getCovid19CountryData(countryCode: string) {
   const filterCovid19CountryData = covid19CountriesDataList.filter(
     country => country.code === countryCode
   )
-  console.log(
-    `filterCovid19CountrData deaths: ${JSON.stringify(
-      filterCovid19CountryData
-    )}`
-  )
   const covid19CountryData: Covid19CountryData = {}
   // covid19CountryData.confirmed = filterCovid19CountrData[0].confirmed,
   // covid19CountryData.suspected = filterCovid19CountrData[0].suspected,
   // covid19CountryData.deaths = filterCovid19CountryData[0].deaths
   // covid19CountryData.totalConfirmed = filterCovid19CountrData[0].totalConfirmed,
-  // const totalDeaths = filterCovid19CountryData[0].totalDeaths
-  console.log(`Muertos totales: ${filterCovid19CountryData[0].totalDeaths}`)
-  covid19CountryData.subtitle = `fallecidos: ${filterCovid19CountryData[0].totalDeaths}`
+  const totalConfirmed = filterCovid19CountryData[0].totalConfirmed
+  const totalDeaths = filterCovid19CountryData[0].totalDeaths
+  covid19CountryData.code = filterCovid19CountryData[0].code
+  covid19CountryData.subtitle = `contagiados: ${totalConfirmed}
+  fallecidos: ${totalDeaths}`
   return covid19CountryData
 }
