dee4c254afc755b9d0d35163546ec3d0eb962725
ignacio68 <ilopez-amor@ya.com>
2020-03-10 20:53:15 +0100

    add firebase init function

--- app/services/firebase/firebase.ts
+++ app/services/firebase/firebase.ts
@@ -1,8 +1,15 @@
-import firebase from 'nativescript-plugin-firebase';
+import firebase from 'nativescript-plugin-firebase'
 
 export function firebaseInit() {
-  firebase.init({
-    persist: true
-  })
+  firebase
+    .init({
+      persist: true
+    })
+    .then(
+      data => {
+        console.log('firebase.init done')
+        return data
+      },
+      error => console.log(`firebase.init error: ${error}`)
+    )
 }
-
