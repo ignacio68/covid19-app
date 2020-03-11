a6b750f8aabbfb0b1bb76ded2b2be3b2709d1289
ignacio68 <ilopez-amor@ya.com>
2020-03-11 20:46:41 +0100

    Add note

--- app/router/index.js
+++ app/router/index.js
@@ -1,21 +1,22 @@
-import AppNavigator from '../views/AppNavigator'
-import Welcome from '../views/Welcome'
-import SignUp from '../views/Auth/SignUp'
-import Charts from '../views/Main/Charts'
-import Info from '../views/Main/Info'
-import Map from '../views/Main/Map'
-import PrivacyPolicy from '../views/Privacy/PrivacyPolicy'
-import TermsOfService from '../views/Privacy/TermsOfService'
+// TODO: Fix the bugs to implement the router
+import AppNavigator from '@/views/AppNavigator'
+import Welcome from '@/views/Welcome'
+import SignUp from '@/views/Auth/SignUp'
+import Charts from '@/views/Main/Charts'
+import Info from '@/views/Main/Info'
+import Map from '@/views/Main/Map'
+import PrivacyPolicy from '@/views/Privacy/PrivacyPolicy'
+import TermsOfService from '@/views/Privacy/TermsOfService'
 
 const routes = {
-  AppNavigator,
-  Welcome,
-  SignUp,
-  Charts,
-  Info,
-  Map,
-  PrivacyPolicy,
-  TermsOfService
+  appNavigator: AppNavigator,
+  welcome: Welcome,
+  signup: SignUp,
+  charts: Charts,
+  info: Info,
+  mapa: Map,
+  privacyPolicy: PrivacyPolicy,
+  termsOfService: TermsOfService
 }
 
 export default routes
