cbb1bab1520a0448ddc8f682a4601de0170543e0
Ignacio <buscemi@ya.com>
2020-03-11 15:01:38 +0100

    add routes

---
+++ app/router/index.js
@@ -0,0 +1,21 @@
+import AppNavigator from '../views/AppNavigator'
+import Welcome from '../views/Welcome'
+import SignUp from '../views/Auth/SignUp'
+import Charts from '../views/Main/Charts'
+import Info from '../views/Main/Info'
+import Map from '../views/Main/Map'
+import PrivacyPolicy from '../views/Privacy/PrivacyPolicy'
+import TermsOfService from '../views/Privacy/TermsOfService'
+
+const routes = {
+  AppNavigator,
+  Welcome,
+  SignUp,
+  Charts,
+  Info,
+  Map,
+  PrivacyPolicy,
+  TermsOfService
+}
+
+export default routes
