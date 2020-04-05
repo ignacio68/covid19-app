b4761995f9476bf1932b238b9b2ed61a2434ed80
ignacio68 <ilopez-amor@ya.com>
2020-04-05 23:10:03 +0200

    Add Tape for testing

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,13 @@ module.exports = function (api) {
   api.cache(true)
 
   return {
-    presets: [['@babel/env', { targets: { esmodules: true, node: 'current' } }]]
+    presets: [['@babel/env', { targets: { esmodules: true, node: 'current' } }]],
+    env: {
+      test: {
+        presets: [['@babel/preset-env', {
+          targets: { node: 'current' }
+        }]]
+      }
+    }
   }
 }
