85436c8ef9c4376f9899baa6d3c6cf76efad5823
ignacio68 <ilopez-amor@ya.com>
2020-04-06 14:24:57 +0200

    refactored to functional programming

--- app/api/index.ts
+++ app/api/index.ts
@@ -2,10 +2,16 @@
 import { setMarker } from '@/services/map'
 import { Place, Covid19Data, Marker } from '@/services/types'
 
-export const setMarkers = (dataPlaces: Place[], covid19DataPlaces: Covid19Data[]) => {
-    const markers: Marker[] = []
-    for (let covid19DataPlace of covid19DataPlaces) {
-      const marker = setMarker(dataPlaces, covid19DataPlaces,  covid19DataPlace.code = '') // FIXME: resolve type conflict
-      // TODO: assign the new marker to markers array
-    }
-  }
+// export const setMarkers = (dataPlaces: Place[], covid19DataPlaces: Covid19Data[]) => {
+//     const markers: Marker[] = []
+//     for (let covid19DataPlace of covid19DataPlaces) {
+//       const codePlace = covid19DataPlace.code<string>
+//       const marker = setMarker({dataPlaces, covid19DataPlaces,  codePlace})
+//       // TODO: assign the new marker to markers array
+//       markers
+//     }
+//   }
+
+export const setMarkers = (dataPlaces: Place[], covid19DataPlaces: Covid19Data[]) => covid19DataPlaces.map(covid19DataPlace => {
+  const codePlace = covid19DataPlace.code as string
+  setMarker({dataPlaces, covid19DataPlaces,  codePlace})})
