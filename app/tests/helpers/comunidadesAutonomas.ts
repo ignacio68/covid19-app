cf817ceb3c64df42644b2da41099ae185aed63f4
ignacio68 <ilopez-amor@ya.com>
2020-04-06 18:11:56 +0200

    Add places database

---
+++ app/tests/helpers/comunidadesAutonomas.ts
@@ -0,0 +1,163 @@
+export type ComunidadAutonoma = {
+  code: string,
+  name: string,
+  name_official: string,
+  lat: string,
+  lng: string,
+  id: string
+}
+
+export const comunidadesAutonomas: ComunidadAutonoma[] = [
+  {
+    code: 'AN',
+    name: 'Andalucía',
+    name_official: 'Andalucía',
+    lat: '37.366667',
+    lng: '-5.983333',
+    id: '01'
+  },
+  {
+    code: 'AR',
+    name: 'Aragón',
+    name_official: 'Aragón',
+    lat: '41.65635',
+    lng: '-0.876566',
+    id: '02'
+  },
+  {
+    code: 'AS',
+    name: 'Asturias',
+    name_official: 'Principado de Asturias',
+    lat: '43.36',
+    lng: '-5.845',
+    id: '03'
+  },
+  {
+    code: 'BA',
+    name: 'Baleares',
+    name_official: 'Illes Balears',
+    lat: '39.566667',
+    lng: '2.65',
+    id: '04'
+  },
+  {
+    code: 'CA',
+    name: 'Canarias',
+    name_official: 'Canarias',
+    lat: '28.15',
+    lng: '-15.416667',
+    id: '05'
+  },
+  {
+    code: 'CN',
+    name: 'Cantabria',
+    name_official: 'Cantabria',
+    lat: '43.45',
+    lng: '-3.8',
+    id: '06'
+  },
+  {
+    code: 'CL',
+    name: 'Castilla y León',
+    name_official: 'Castilla y León',
+    lat: '41.633333',
+    lng: '-4.716667',
+    id: '07'
+  },
+  {
+    code: 'CM',
+    name: 'Castilla-La Mancha',
+    name_official: 'Castilla-La Mancha',
+    lat: '39.856528',
+    lng: '-4.025111',
+    id: '08'
+  },
+  {
+    code: 'CT',
+    name: 'Cataluña',
+    name_official: 'Cataluña',
+    lat: '41.383333',
+    lng: '2.166667',
+    id: '09'
+  },
+  {
+    code: 'CV',
+    name: 'Valencia',
+    name_official: 'Comunitat Valenciana',
+    lat: '39.47',
+    lng: '-0.376667',
+    id: '10'
+  },
+  {
+    code: 'CE',
+    name: 'Ceuta',
+    name_official: 'Ceuta',
+    lat: '35.888056',
+    lng: '-5.316111',
+    id: '18'
+  },
+  {
+    code: 'EX',
+    name: 'Extremadura',
+    name_official: 'Extremadura',
+    lat: '38.9',
+    lng: '-6.333333',
+    id: '11'
+  },
+  {
+    code: 'GA',
+    name: 'Galicia',
+    name_official: 'Galicia',
+    lat: '42.866667',
+    lng: '-8.55',
+    id: '12'
+  },
+  {
+    code: 'LR',
+    name: 'La Rioja',
+    name_official: 'La Rioja',
+    lat: '42.464653',
+    lng: '-2.4458',
+    id: '17'
+  },
+  {
+    code: 'MA',
+    name: 'Madrid',
+    name_official: 'Comunidad de Madrid',
+    lat: '40.433333',
+    lng: '-3.683333',
+    id: '13'
+  },
+  {
+    code: 'ME',
+    name: 'Melilla',
+    name_official: 'Melilla',
+    lat: '35.291667',
+    lng: '-2.938333',
+    id: '19'
+  },
+  {
+    code: 'MU',
+    name: 'Murcia',
+    name_official: 'Región de Murcia',
+    lat: '37.986111',
+    lng: '-1.130278',
+    id: '14'
+  },
+  {
+    code: 'NA',
+    name: 'Navarra',
+    name_official: 'Comunidad Foral de Navarra',
+    lat: '42.818333',
+    lng: '-1.644167',
+    id: '15'
+  },
+  {
+    code: 'PV',
+    name: 'País Vasco',
+    name_official: 'País Vasco',
+    lat: '42.85',
+    lng: '-2.683333',
+    id: '16'
+  }
+]
