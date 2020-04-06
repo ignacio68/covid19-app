be312d0aff85755d42f70d167a3673d2c9125f6a
ignacio68 <ilopez-amor@ya.com>
2020-04-06 17:41:07 +0200

    remove unusued commentaries

--- app/services/places.ts
+++ app/services/places.ts
@@ -1,18 +1,13 @@
-// import { countriesList } from '@/repository'
-// import { comunidadesAutonomas } from  '@/repository'
 import { Place } from './types'
 
-const filterPlaceWithPlaceCode = (dataPlaces: Place[], codePlace: string) => dataPlaces.filter(place => place.code === codePlace)
+export const filterPlaceWithPlaceCode = (dataPlaces: Place[], codePlace: string) => dataPlaces.filter(place => place.code === codePlace)
 
 export const getDataPlace = (dataPlaces: Place[], codePlace: string):Place => {
-  // const filterCountry = comunidadesAutonomas.filter(country => country.code === codePlace)
   const filterCountry = filterPlaceWithPlaceCode(dataPlaces, codePlace)
   const placeData: Place = {
     lat: filterCountry[0].lat,
     lng: filterCountry[0].lng,
     title: filterCountry[0].name
-    // onTap = placeData => console.log(`El pais elegido es: ${placeData.name}`)
   }
-  
   return placeData
 }
